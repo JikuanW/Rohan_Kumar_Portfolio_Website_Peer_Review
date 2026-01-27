88f506047c38d9e849de3f032baddd6f1f46c1bd
rohannes220 <121151761+rohannes220@users.noreply.github.com>
2026-01-26 22:33:49 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function openTravel() {
+    const travelVideo = "https://www.youtube.com/watch?v=SVjOB3ZN1Ek";
+    window.open(travelVideo, "_blank");
+}
+
+function openSoccer() {
+    const soccerVideo = "https://www.youtube.com/watch?v=8Ij7A45_sVM";
+    window.open(soccerVideo, "_blank");
+}
+
+function openCooking() {
+    const cookingVideo = "https://www.youtube.com/watch?v=bnYA2qIlPCQ";
+    window.open(cookingVideo, "_blank");
+}
